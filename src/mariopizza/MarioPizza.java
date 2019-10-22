e8014333b432ae9f71d63a11641646b664c666b5
Rasmus Hansen <rasmush22@live.dk>
2019-10-22 18:26:08 +0200

    Made pizza test data

--- src/mariopizza/MarioPizza.java
+++ src/mariopizza/MarioPizza.java
@@ -17,10 +17,21 @@ public class MarioPizza {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Test data
+        pizzaTestData pizza1 = new pizzaTestData(1,"Tomat","","","","","");
+        pizzaTestData pizza2 = new pizzaTestData(1,"Tomat","ost","","","","");
+        pizzaTestData pizza3 = new pizzaTestData(1,"Tomat","ost","skinke","","","");
+        pizzaTestData pizza4 = new pizzaTestData(1,"Tomat","ost","skinke","ananas","","");
+        pizzaTestData pizza5 = new pizzaTestData(1,"Tomat","ost","skinke","ananas","pepperfrugt","");
+        pizzaTestData pizza6 = new pizzaTestData(1,"Tomat","ost","skinke","ananas","pepperfrugt","Pepperoni");
         
-        pizzaTestData pizza1 = new pizzaTestData();
-        
-        
+        System.out.println(pizza1);
+        System.out.println(pizza2);
+        System.out.println(pizza3);
+        System.out.println(pizza4);
+        System.out.println(pizza5);
+        System.out.println(pizza6);
+        //Test data slut
         
         
     }
